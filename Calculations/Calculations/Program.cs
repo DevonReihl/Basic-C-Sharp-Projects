0a0874acea5ccbecd2adba07cfaf8eb10dd74c14
Devon <Dreihlmama@gmail.com>
2019-06-07 20:49:21 -0700

    Math equations
    Get User to give a number and do math calculations on them

---
+++ Calculations/Calculations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number?");
+            int product = Convert.ToInt32(Console.ReadLine()) * 50;
+            Console.WriteLine("Your number multiplied by 50 is " + product.ToString());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Please give me another number?");
+            int addition = Convert.ToInt32(Console.ReadLine()) + 25;
+            Console.WriteLine("Your number plus 25 is " + addition.ToString());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Please give me another number?");
+            double quotient = Convert.ToDouble(Console.ReadLine()) / 12.5;
+            Console.WriteLine("Your divided by 12.5 is " + quotient.ToString());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Another number please?");
+            bool trueFalse = Convert.ToInt32(Console.ReadLine()) > 50;
+            Console.WriteLine("Is your number greater then 50? " + trueFalse.ToString());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("One last number please");
+            int remainder = Convert.ToInt32(Console.ReadLine()) % 7;
+            Console.WriteLine("Your number divided by 7 has a remainder of " + remainder.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATHandComparisonOperators
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine("Ten multiplied by five = " + product.ToString());
+            //Console.ReadLine();
+
+            //int  quotient= 10 / 5;
+            //Console.WriteLine("Ten divided by five = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 10 % 3;
+            //Console.WriteLine("The remainder of 10 divided by 3 = " + remainder.ToString());
+            //Console.ReadLine();
+
+            //int oddEven = 10 % 2;
+            //Console.WriteLine("The remainder of 10 divided by 2 = " + oddEven.ToString());
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 72;
+
+            //bool isWarm = currentTemperature > roomTemperature;
+            //bool isWarm = currentTemperature >= roomTemperature;
+            //bool isWarm = currentTemperature == roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
