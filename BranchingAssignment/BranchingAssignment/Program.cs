f9ea0bd73f43189821138d38f2c27cc7907f4668
Devon <Dreihlmama@gmail.com>
2019-06-09 15:10:24 -0700

    Branching
    Branching Demo and Assignment

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.WriteLine("Please enter you package weight now.");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter you package height now.");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter you package length now.");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+
+                int packageDimensions = (packageWeight + packageLength + packageHeight);
+                if (packageDimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                }
+                else
+                {
+                    int quote = packageDimensions * packageWeight / 100;
+                    Console.WriteLine("Your estimated shipping cost is $" + quote);
+                    Console.ReadLine();
+                }
+            
+            }
+            Console.ReadLine();
+
+            //Console.WriteLine("Please enter you package height now.");
+            //int packageHeight = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter you package length now.");
+            //int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            //int packageDimensions = (packageWeight + packageLength + packageHeight);
+
+            //if (packageWeight > 50)
+            //{
+            //    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            //}
+            //else if (packageDimensions > 50)
+            //{
+            //    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+            //}
+            //else
+            //{
+            //    int quote = packageDimensions * packageWeight / 100;
+            //    Console.WriteLine("Your estimated shipping cost is $" + quote);
+            //    Console.ReadLine();
+            //}
+        }
+    }
+}
+
---
+++ BranchingExample/BranchingExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int currentTemperature = 70;
+            int roomTemperature = 70;
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi " + name + ", what is the temperature outside where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (roomTemperature > currentTemperature)
+            {
+                Console.WriteLine("It is colder than room temperature");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("OPPS we have a problem Houston!!!");
+            }
+
+            //One line if/else statment
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResult);
+            Console.ReadLine();
+
+        }
+    }
+}
