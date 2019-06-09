53faadc8756cfc44bf9f27bfd104431cad95027e
Devon <Dreihlmama@gmail.com>
2019-06-09 11:33:00 -0700

    Add BooleanLogic
    Assignment complete

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? Please enter Yes or No");
+            bool qualifyDui = Console.ReadLine() == "No";
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            Console.WriteLine("Your age is: " +age);
+            Console.WriteLine("You have not had a DUI: " +qualifyDui);
+            Console.WriteLine("You have had " +tickets+ " speeding tickets.");
+            bool qualifyAge = Convert.ToInt32(age) > 15;
+            bool qualifyTickets = Convert.ToInt32(tickets) <= 3;
+            Console.WriteLine("Does applicant qualify?");
+            Console.WriteLine(qualifyAge && qualifyDui && qualifyTickets);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ booleanOperators/booleanOperators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booleanOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND OPERATOR
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true && true);
+
+            //OR OPERATOR
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+            Console.WriteLine(true || false);
+
+            //EQUALS OPERATOR
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            //IS NOT OPERATOR
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            //ONE BUT NOT BOTH ARE TRUE
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            Console.ReadLine();
+        }
+    }
+}
