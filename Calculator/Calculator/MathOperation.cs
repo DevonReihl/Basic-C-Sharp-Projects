b45d80475933aa508a63b171232517e6e664dce8
Devon <Dreihlmama@gmail.com>
2019-06-15 13:36:44 -0700

    BasicCalculator
    Program that adds, subtracts, multiples and divides

---
+++ Calculator/Calculator/MathOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class mathOperation
+    {
+        public static int add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int minus(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int times(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask for user input
+            Console.WriteLine("Please give me a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please give me another number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Call methods
+            Console.WriteLine("If we add your numbers you get: " + mathOperation.add(num1, num2));
+            Console.WriteLine("If we subtrate your numbers you get: " + mathOperation.minus(num1, num2));
+            Console.WriteLine("If we multiple your numbers you get: " + mathOperation.times(num1, num2));
+            Console.WriteLine("If we divide your numbers you get rounded: " + mathOperation.divide(num1, num2));
+
+            Console.ReadLine();
+        }
+    }
+}
