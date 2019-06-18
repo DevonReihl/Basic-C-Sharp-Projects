b8291c829663174565f5ab39a61efba709a3ec29
Devon <Dreihlmama@gmail.com>
2019-06-18 13:02:11 -0700

    evenOrOdd
    Created class with method to divide user input by 2. evaluate if even or odd and then give examples

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -8,6 +8,9 @@ namespace TwentyOne
 {
     public class Deck
     {
+        //=========================================
+        // Properties of this class (constructor)
+        //=========================================
         public Deck()
         {
             Cards = new List<Card>();
@@ -27,6 +30,61 @@ namespace TwentyOne
             }
             
         }
+        //=========================================
+        //Property accessors
+        //=========================================
         public List<Card> Cards { get; set; }
+        //=========================================
+        //Methods of this class
+        //=========================================
+
+        //=========================================
+        //Static vs not
+        //=========================================
+        //public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+        //{
+        //    timesShuffled = 0;
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        timesShuffled++;
+        //        List<Card> TempList = new List<Card>();
+        //        Random random = new Random();
+
+        //        while (deck.Cards.Count > 0)
+        //        {
+        //            int randomIndex = random.Next(0, deck.Cards.Count);
+        //            TempList.Add(deck.Cards[randomIndex]);
+        //            deck.Cards.RemoveAt(randomIndex);
+        //        }
+        //        deck.Cards = TempList;
+        //    }
+        //    return deck;
+
+        //}
+
+        public void Shuffle( int times = 1)
+        {
+            
+            for (int i = 0; i < times; i++)
+            {
+               
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+                //this.Cards = TempList; // optional was to do it
+            }
+            //=========================================
+            //no need to return anymore
+            //=========================================
+            //return deck;
+
+        }
     }
 }
---
+++ evenOrOdd/evenOrOdd/evenOrOdd.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenOrOdd
+{
+    class evenOrOdd
+    {
+        public void calculateEvenOrOdd(int num)
+        {
+            int results = num / 2;
+            int results2 = num % 2;
+
+            if (results2 == 0)
+            {
+                Console.WriteLine("Your number divided by two is: " + results);
+                Console.WriteLine("You entered an even number.");
+
+                void evenExamples(out string message, out int num1, out int num2, out int num3, out int num4)
+                {
+
+                    message = "Like:";
+                    num1 = 2;
+                    num2 = 4;
+                    num3 = 6;
+                    num4 = 8;
+                }
+                string argMessage;
+                int argNumber;
+                int argNumber1;
+                int argNumber2;
+                int argNumber3;
+
+
+                evenExamples(out argMessage, out argNumber, out argNumber1, out argNumber2, out argNumber3);
+
+                Console.WriteLine(argMessage);
+                Console.WriteLine(argNumber);
+                Console.WriteLine(argNumber1);
+                Console.WriteLine(argNumber2);
+                Console.WriteLine(argNumber3);
+
+            }
+            else
+            {
+                Console.WriteLine("Your number divided by two is: " + results);
+                Console.WriteLine("You entered an odd number.");
+                void oddExamples(out string message, out int oddNum, out int oddNum1, out int oddNum2, out int oddNum3)
+                {
+
+                    message = "Like:";
+                    oddNum = 1;
+                    oddNum1 = 3;
+                    oddNum2 = 5;
+                    oddNum3 = 7;
+                }
+                string argMessage;
+                int argNumber;
+                int argNumber1;
+                int argNumber2;
+                int argNumber3;
+
+
+                oddExamples(out argMessage, out argNumber, out argNumber1, out argNumber2, out argNumber3);
+
+                Console.WriteLine(argMessage);
+                Console.WriteLine(argNumber);
+                Console.WriteLine(argNumber1);
+                Console.WriteLine(argNumber2);
+                Console.WriteLine(argNumber3);
+            }
+        }
+
+    }
+}
