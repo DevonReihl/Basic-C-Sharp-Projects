4b9534ab7ac3c409839b68c77a49dc5343f15cda
Devon <Dreihlmama@gmail.com>
2019-06-17 22:41:04 -0700

    Method Overloading
    Created methods that are called by different types of variables

---
+++ variableTypeMethods/variableTypeMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variableTypeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            decimal num2 = 12;
+            string num3 = "12";
+
+            variableTypes multiply = new variableTypes();
+            Console.WriteLine("The integer times 10 is: " + multiply.mathOp(num));
+            Console.ReadLine();
+
+            variableTypes divide = new variableTypes();
+            Console.WriteLine("the decimal number divide by 2 is: " + divide.mathOp(num2));
+            Console.ReadLine();
+
+            variableTypes addition = new variableTypes();
+            Console.WriteLine("Adding  100 to your string equals: " + addition.mathOp(num3));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ variableTypeMethods/variableTypeMethods/variableTypes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variableTypeMethods
+{
+    class variableTypes
+    {
+        public int mathOp(int num)
+        {
+            int answer = num * 10;
+            return answer;
+        }
+        public decimal mathOp (decimal num2)
+        {
+            decimal answer2 = num2 / 2;
+            return answer2;
+        }
+        public int mathOp(string num3)
+        {
+            int answer3 = Convert.ToInt32(num3) + 100;
+            return answer3;
+        }
+
+    }
+}
