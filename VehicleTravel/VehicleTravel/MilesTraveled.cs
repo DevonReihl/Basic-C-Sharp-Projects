9858ac0fdeb376e07a63ab7d2084160d6893d10f
Devon <Dreihlmama@gmail.com>
2019-06-17 19:06:03 -0700

    Changed
    Added to TwentyOne per next video.
    Step 113 started

---
+++ VehicleTravel/VehicleTravel/MilesTraveled.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTravel
+{
+    public class MilesTraveled
+    {
+        
+
+        //Calculate how many hours they have driven
+        public static void milesTraveled(int speed, int miles)
+        {
+            float hours = miles / speed;
+            Console.WriteLine(speed);
+        }
+        
+    }
+}
---
+++ VehicleTravel/VehicleTravel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTravel
+{
+    class Program
+    {
+        public Program() // instantiates the class??
+        {
+            MilesTraveled milesTraveled = new MilesTraveled();
+            float hours = 0;
+
+
+        }
+
+    }
+}
+
+}
