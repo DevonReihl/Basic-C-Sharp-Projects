189fffbfd30ecb72bcc06614c8c45f03ba19b0a0
Devon <Dreihlmama@gmail.com>
2019-06-19 12:33:08 -0700

    Abstract revisions
    Added Abstract class to TwentyOne and Idnetification

--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class Game
+    public abstract class Game // basic another word
     {
         public List<string> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
 
-        public void ListPlayers()
+        public abstract void Play();
+
+        public virtual void ListPlayers() // virtual means method inherited by other class but can be overwritten
         {
             foreach( string player in Players)
             {
