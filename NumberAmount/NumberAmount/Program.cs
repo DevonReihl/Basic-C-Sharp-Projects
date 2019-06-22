73aa18b4c7a716ac81d274d5162716e208907e9e
Devon <Dreihlmama@gmail.com>
2019-06-21 19:06:59 -0700

    NumberAmount
    Created struct of number and added  a decimal to amount

---
+++ NumberAmount/NumberAmount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAmount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number value = new Number();
+            value.Amount = 300000000000000000;
+
+
+            Console.WriteLine("The amount is: " + value.Amount);
+            Console.ReadLine();
+        }
+    }
+}
