e9935f7430ebcae896020ac596ee9a0b74bb666e
Devon <Dreihlmama@gmail.com>
2019-06-26 14:45:35 -0700

    Original Text File
    Fixed original project by dublicating old version on Github

--- TwentyOne/Casino/Dealer.cs
+++ TwentyOne/Casino/Dealer.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace TwentyOne
+namespace Casino
 {
     public class Dealer
     {
@@ -21,6 +21,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\user\Desktop\TechAcademy\C#-.net\The-Tech-Academy-Basic-C-Sharp-Projects\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -1,29 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //string text = "here is some text.";
-            //File.WriteAllText(@"C:\Users\user\Desktop\TechAcademy\C#-.net\The-Tech-Academy-Basic-C-Sharp-Projects\log.txt", text);
-
-            //string text = File.ReadAllText(@"C:\Users\user\Desktop\TechAcademy\C#-.net\The-Tech-Academy-Basic-C-Sharp-Projects\log.txt");
-
-            //Date time and timestamp examples
-            DateTime dateTime = new DateTime(2018, 6, 26);
-            DateTime yearOfBirth = new DateTime(2000, 6, 26);
-            DateTime yearOfGraduation = new DateTime(2013, 5, 15);
-
-            TimeSpan ageAtGraduation = yearOfGraduation - yearOfBirth;
-
-
+        {    
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by giving me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("Hi, {0} and how much money did you bring to play with today?", playerName);
